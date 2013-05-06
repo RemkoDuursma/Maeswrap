2a75246bbbc28121050ab9d0f5f877cbc4db552e
remkoduursma <remkoduursma@gmail.com>
2013-05-07 09:47:22 +1000

    Further facelift of readPAR. Improved regexp.

--- R/readPAR.R
+++ R/readPAR.R
@@ -3,19 +3,21 @@ function(datfile, parname, namelist=NA,fail=TRUE){
 
 	# read the file
 	dat_lines <- str_trim(tolower(readLines(datfile)))
-	parname <- paste0("^", tolower(parname))
-	if(!is.na(namelist))namelist <- tolower(namelist)
+	
+  # regexp
+  parreg <- paste0("^", tolower(parname), "[[:blank:]]+=")
+	
+  if(!is.na(namelist))namelist <- tolower(namelist)
 
 	# If namelist is not NA, find the parameter within some namelist.
 	# (otherwise find the parameter by its name).
-	namelist_loc <- 0
 	if(!is.na(namelist)){
 
 		nl <- paste0("&", namelist, "$")
 		namelist_loc <- grep(nl, dat_lines)
 		if(length(namelist_loc)==0){
 			if(fail)
-        stop("Can't find namelist\n")
+        stop("Can't find namelist ", namelist)
 			else
         return(NA)
 		}
@@ -28,18 +30,21 @@ function(datfile, parname, namelist=NA,fail=TRUE){
   
 		# nth element of the namelist
 		# if values separated by /n, here separate elements of the vector!
-		parloc <- grep(parname, datlines_namelist) #+ namelist_loc - 1
+		parloc <- grep(parreg, datlines_namelist) #+ namelist_loc - 1
 
     if(length(parloc)==0){
 			if(fail)
-        stop(paste("Cannot find",parname,"in",datfile,"in the namelist",namelist,"\n"))
+        stop(paste("Cannot find",parname,"in",datfile,
+                   "in the namelist",namelist,"\n"))
 			else
         return(NA)
 		}
 
-    
     nmlsubs <- datlines_namelist[parloc:(length(datlines_namelist)-1)]
     parl <- grep("=", nmlsubs)
+    
+    # Include next lines within namelist if they don't have an = in it,
+    # which means a \n was added in the file (which is allowed!)
     if(length(parl) > 1)
       nmlpar <- nmlsubs[1:(parl[2]-1)]
     else
@@ -52,21 +57,14 @@ function(datfile, parname, namelist=NA,fail=TRUE){
 	# only parameter name provided
 	if(is.na(namelist)){
     
-    parLocs <- grep("=",dat_lines)
-    parNames <- strsplit(dat_lines[parLocs],"=")
-    parNames <- str_trim(sapply(parNames, "[", 1))
+    parloc <- grep(parreg, dat_lines)
     
-    parloc <- grep(paste0(parname,"$"), parNames)
-  
 		if(length(parloc)==0){
 			if(fail)
         stop("Cannot find ",parname," in ",datfile)
 			else
         return(NA)
 		}
-    
-    # convert to original line number
-    parloc <- parLocs[parloc]
     
     nmlsubs <- dat_lines[parloc:(length(dat_lines)-1)]
     parl <- grep("=", nmlsubs)
