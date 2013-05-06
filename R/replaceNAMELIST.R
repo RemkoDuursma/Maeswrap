124669a1f6513d520d63ce7bf16a522da47ce6d1
remkoduursma <remkoduursma@gmail.com>
2013-05-07 09:57:50 +1000

    Facelift of replaceNAMELIST (functionality unchanged)

--- R/replaceNAMELIST.R
+++ R/replaceNAMELIST.R
@@ -6,49 +6,42 @@ function(namelist=NA,
     if(is.na(datfile))stop("Must provide name of the .dat file.")
     
     # Find NAMELIST and end of it ("/")
-    datfile_lines <- tolower(readLines(datfile))
-	namelist <- tolower(namelist)
+    datlines <- str_trim(tolower(readLines(datfile)))
+	  namelist <- tolower(namelist)
+    nmreg <- paste0("&",namelist)
 	
-    namelist_loc <- length(datfile_lines)
-    for(i in 1:length(datfile_lines))if(str_trim(datfile_lines[i]) == paste("&",namelist,sep=""))namelist_loc <- i
-        
-    namelist_end <- NA
-    k <- namelist_loc
-    if(!(namelist_loc == length(datfile_lines))){
-        while(is.na(namelist_end)){
-         if(str_trim(datfile_lines[k]) == "/")namelist_end <- k
-         k <- k + 1
-        }
-        # Part of original file before and after this namelist.
-        datfile_before <- datfile_lines[1:(namelist_loc-1)]
-		
-		if((namelist_end+1) < length(datfile_lines)){
-			datfile_after <- datfile_lines[(namelist_end+1):length(datfile_lines)]
-		} else {
-			datfile_after <- ""
-		}
-		
-    } else {    # if namelist not found, it will be put at end of file.
-        datfile_before <- datfile_lines
-        datfile_after <- ""
+    nl_start <- grep(nmreg, datlines)
+    
+    # Find nearest namelist closer ("/")
+    endnml <- grep("^/$", datlines)
+    nmllen <- min(endnml[endnml > nl_start] - nl_start)
+    nl_end <- nl_start + nmllen
+    
+    datlines_namelist <- datlines[nl_start:(nl_start + nmllen)]
+    
+    # Part of original file before and after this namelist.
+    if(nl_start > 1){
+      pref <- datlines[1:(nl_start-1)]
+    } else {
+      pref <- ""
     }
-
+    if((nl_end+1) < length(datlines)){
+      postf <- datlines[(nl_end+1):length(datlines)]
+    } else {
+      postf <- ""
+    }
+    
     # New namelist
     listvals <- c()
-    for(i in 1:length(vals))listvals[i] <- paste(names(vals)[i],printme(vals[[i]], "\n"), sep=" = ")
-    newlist <- c(   paste("&",namelist,sep=""),
-                    listvals,   
-                    "/"
-                    )
-					
-    # Rewrite file
-    Lines <- c(datfile_before, newlist, datfile_after)
-    nrlines <- length(Lines)
-    write(Lines[1], file=datfile)
-    for (i in 2:nrlines) {
-	   write(Lines[i], file=datfile, append=TRUE)
+    for(i in 1:length(vals)){
+      listvals[i] <- paste(names(vals)[i],printme(vals[[i]], "\n"), 
+                           sep=" = ")
+      newlist <- c(paste0("&",namelist),
+                    listvals, "/")
     }
-    write("\n", file=datfile, append=TRUE)
-                    
+    
+    # Rewrite file
+    Lines <- c(pref, newlist, postf)
+    writeLines(Lines, datfile) 
 }
 
