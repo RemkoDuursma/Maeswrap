295585bb3fa8cbb68b696257de28203f2e36b950
remkoduursma <remkoduursma@gmail.com>
2013-05-07 10:02:38 +1000

    Tidying

--- R/readhrflux.R
+++ R/readhrflux.R
@@ -6,10 +6,7 @@ function(filename="hrflux.dat"){
 
 	hrflux <- read.table(filename, skip=colloc,na.strings='NaN')
 	names(hrflux) <- delempty(strsplit(delempty(strsplit(hrlines[colloc], "Columns:")[[1]])," ")[[1]])
-	 
-	 
-	#names(hrflux) <- c("DOY", "Tree", "HOUR", "hrPAR", "hrNIR", "hrTHM", "hrPs", "hrRf", 
-	#					"hrRmW", "hrLE", "LECAN", "Gscan", "hrH", "TCAN", "VPD")
+
 	hrflux$conttime <- with(hrflux, DOY + HOUR/max(HOUR))
 	
 	hrflux
--- R/readmet.R
+++ R/readmet.R
@@ -1,16 +1,13 @@
-`readmet` <-
-function(filename="met.dat", nlines=-1){
+readmet <- function(filename="met.dat", nlines=-1){
 
 	metlines <- readLines(filename)
 	namesloc <- grep("^columns",tolower(metlines))
-	# namesloc <- setdiff(namesloc,grep("nocolumns",metlines))
-	
 	namesline <- metlines[namesloc]
 	datastart <- grep("DATA STARTS", metlines)
 	sp <- strsplit(namesline, "=")[[1]][2]
 	NAMES <- delempty(str_trim(strsplit(sp, "\t")[[1]]))
 	NAMES <- gsub("'", "", NAMES)
-	NAMES <- do.call("c", strsplit(NAMES, " ", fixed=TRUE))
+	NAMES <- unlist(strsplit(NAMES, " ", fixed=TRUE))
 	metdata <- read.table(filename, header=FALSE, skip=datastart, nrows=nlines)
 	names(metdata) <- NAMES
 	names(metdata)[names(metdata) == "RH%"] <- "RHperc"
