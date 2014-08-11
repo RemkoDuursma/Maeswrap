e42c953784424dcb9c5ee7f4c879aeb79150262d
Remko Duursma <remkoduursma@gmail.com>
2014-08-11 14:27:44 +1000

    readuspar works again and is exported

--- R/readuspar.R
+++ R/readuspar.R
@@ -1,8 +1,15 @@
-readuspar <- function(){
+#' Read understorey simulations
+#' @description Reads the point-wise output file when the understorey was simulated.
+#' @param filename The understorey file
+#' @export
+readuspar <- function(filename="uspar.dat"){
 
-	dataset <- read.table("uspar.dat", header=TRUE)
-	if(ncol(dataset) != 12)stop("Format of uspar.dat has changed\n")
-    names(dataset) <- c("day","hour","point","x","y","z","beam","diff","ipar","apar","psus","etus")
-	
-return(dataset)
+  watlines <- readLines(filename, 100)
+  colloc <- grep("Columns",watlines)
+  namesline <- watlines[colloc]
+  NAMES <- delempty(strsplit(strsplit(namesline, ":")[[1]][2], " ")[[1]])
+  uspar <- read.table(filename, header=FALSE, na.strings="-999.0000", skip=colloc)
+  names(uspar) <- NAMES
+
+return(uspar)
 }
