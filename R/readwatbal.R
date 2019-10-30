99fa4631de3977883207e8ac9d91850f8eaab2b4
remi.vezy <VEZY@users.noreply.github.com>
2019-10-30 13:31:34 +0100

    Add readwatbalday

--- R/readwatbal.R
+++ R/readwatbal.R
@@ -21,16 +21,47 @@
 #' }
 #' @export
 readwatbal <- function (filename = "watbal.dat",...){
-  
-  if(!require(data.table)){"Please download data.table package"}
   watlines <- readLines(filename,100)
   colloc <- grep("Columns", watlines)
   namesline <- watlines[colloc]
-  NAMES <- delempty(trim(strsplit(strsplit(namesline, ":")[[1]][2],
-                                  " ")[[1]]))
+  NAMES <- delempty(stringr::str_trim(strsplit(strsplit(namesline, ":")[[1]][2]," ")[[1]]))
   watbal <- data.table::fread(filename, header = FALSE, na.strings = "-999.0000", 
-                              skip = colloc+1, data.table = F,...)
+                              skip = colloc+1, data.table = FALSE,...)
   names(watbal) <- NAMES
   return(watbal)
 }
 
+
+
+#' Reads the watbalday.dat MAESPA output file
+#'
+#' @description Reads the daily water balance output file ("watbalday.dat").
+#'
+#'
+#' @param filename Default name of the daily water balance output file.
+#' @return Returns a dataframe.
+#' @author Rémi Vezy
+#' @keywords utilities
+#' @examples
+#'
+#' \dontrun{
+#'
+#' # Simple as this:
+#' mywatbalresult <- readwatbal()
+#'
+#' # If you want to select the water balance file with a menu:
+#' readwatbalday(file.choose())
+#'
+#' }
+#' @export
+readwatbalday <- function (filename = "watbalday.dat") 
+{
+  watlines <- readLines(filename)
+  colloc <- grep("Columns", watlines)
+  namesline <- watlines[colloc]
+  NAMES <- delempty(trim(strsplit(strsplit(namesline, ":")[[1]][2]," ")[[1]]))
+  watbal <- data.table::fread(filename, header = FALSE, na.strings = "-999.0000",
+                              skip = colloc, data.table = FALSE)
+  names(watbal) <- NAMES
+  watbal
+}
