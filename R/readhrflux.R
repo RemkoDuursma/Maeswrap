9458d9a719cdcc47dcf4c312931c960a7d4d71f6
remi.vezy <VEZY@users.noreply.github.com>
2019-10-30 11:39:41 +0100

    Update readhrflux.R
    Much faster using data.table:fread: microbenchmark : Time/3 in average compared to previous

--- R/readhrflux.R
+++ R/readhrflux.R
@@ -16,16 +16,13 @@
 #' mysim2 <- readhrflux()
 #' }
 #' @export
-readhrflux <- function(filename="hrflux.dat"){
-
-	hrlines <- readLines(filename, 100)
-	colloc <- grep("Columns",hrlines)
-
-	hrflux <- utils::read.table(filename, skip=colloc,na.strings='NaN')
-	names(hrflux) <- delempty(strsplit(delempty(strsplit(hrlines[colloc], "Columns:")[[1]])," ")[[1]])
-
-	hrflux$conttime <- with(hrflux, DOY + HOUR/max(HOUR))
-
-	hrflux
+readhrflux= function (filename = "hrflux.dat",...){
+  hrlines <- readLines(filename, n= 50)
+  colloc <- grep("Columns", hrlines)
+  hrflux <- data.table::fread(filename, skip = colloc, na.strings = "NaN", data.table = F, ...)
+  names(hrflux) <- delempty(strsplit(delempty(strsplit(hrlines[colloc], "Columns:")[[1]]), " ")[[1]])
+  hrflux$conttime <- hrflux$DOY + hrflux$HOUR/max(hrflux$HOUR)
+  
+  hrflux
 }
 
